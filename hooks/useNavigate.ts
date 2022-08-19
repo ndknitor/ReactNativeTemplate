e2dc1a6147817485eb7aaaaf06437c316b154da6
kn <nguyentuanpkn@gmail.com>
2022-08-19 14:57:18 +0700

    Delete useNavigate

--- hooks/useNavigate.ts
+++
@@ -1,8 +0,0 @@
-import { ParamListBase, useNavigation } from "@react-navigation/native";
-import { StackNavigationProp } from "@react-navigation/stack";
-
-export default function useNavigate()
-{
-    const navigation = useNavigation<StackNavigationProp<ParamListBase>>();
-    return  navigation;
-}
